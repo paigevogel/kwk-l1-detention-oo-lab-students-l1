5868c234471073afe23cd7f8a7263e3f692c72f5
paigevogel <paigevogel36@gmail.com>
2018-06-07 21:36:44 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,1 +1,12 @@
-#Code your detention class here
+class Detention
+  
+  attr_accessor :location
+  
+  def initialize(location)
+    @location=location
+  end
+end
+
+d = Detention.new("room 301")
+
+puts d.location
